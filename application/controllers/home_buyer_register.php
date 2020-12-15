237f3b89e72a86068edb707bad78679761cfd9d0
Preeti <preetisahu0706@gmail.com>
2020-12-15 15:31:05 +0530

    rawmet

--- application/controllers/home_buyer_register.php
+++ application/controllers/home_buyer_register.php
@@ -45,7 +45,7 @@ class Home_buyer_register extends CI_Controller {
 		
 		$datainserr = "Data Inserted Successfully";
 		$status = $this->Admin_model->insert('buyer_register', $data2);
-		header('location: '.base_url().'home/'.$datainserr);
+		header('location: '.base_url().'home/index/'.$datainserr);
 		  
         }
         
