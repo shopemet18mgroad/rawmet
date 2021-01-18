ad46146c3353d0f10afb0d2ec4d750113feb7112
megha <meghasangmesh2@gmail.com>
2021-01-18 18:45:49 +0530

    changes by megha7

--- application/views/customer/sellnego.php
+++ application/views/customer/sellnego.php
@@ -28,43 +28,72 @@
 			<th>Category</th>
 			<th>Company Name</th>
 			<th>Supplier Price</th>
-			<th>Available Stocks</th>
-			<th>Location</th>
+		     <th>Location</th>
 			<th>Buyer Quantity</th>
 			<th>Buyer Price</th>
 			<th>View Quotation</th>
-			<th>Send Purchase Order</th>
+			
       </tr>
     </thead>
     <tbody>
+	<form action="<?php echo base_url();?>Customer_spurchase_order" method="POST" id="upload-form" enctype="multipart/form-data">
+	<?php $k=0;?>
 	 <?php foreach($sqldata as $row){?>
       <tr>
 	  <?php $proid = str_ireplace('/','-',$row->productid);
 				?>
-			<td><?php echo $row->vname;?></td>
-			<td><?php echo $row->busername;?></td>
-			<td><?php echo $row->productname;?></td>
-			<td><?php echo $row->productid;?></td>
-			<td><?php echo $row->category;?></td>
-			<td><?php echo $row->companyname;?></td>
-			<td><?php echo $row->price;?></td>
-			<td><?php echo $row->supplyability;?></td>
-			<td><?php echo $row->pcities;?></td>
-			<td><?php echo $row->bquantity."/"; echo $row->bunits;?></td>
-			<td><?php echo $row->bprice."/"; echo $row->bsupplyability;?></td>
-			<td><a href="<?php echo base_url().'web_files/uploads/'.$proid[0]?>" target="_blank">
-				
-			<a href="<?php echo base_url().'Customer_sellnegoquotation/auc_no/'.urldecode($row->vname)."/".urldecode($row->busername);?>" target="_blank">
-				<i class="fa fa-download" aria-hidden="true"></i></a></td>
+				<?php $prodid = str_ireplace('/','-',$row->productid);?>
+		
 			
-				<td><input class="form-group w-auto"  multiple="multiple"  type="file" name="uploadporder">
-				<input type="submit" id="" class="btn btn-primary " name="submit" value="Upload">
-		 </td>
+		
+			<td><input type="hidden" name="vname" value="<?php echo $row->vname;?>">
+							<?php echo $row->vname;?></td>
+			<td><input type="hidden" name="busername" value="<?php echo $row->busername;?>">
+							<?php echo $row->busername;?></td>
+			<td><input type="hidden" name="productname" value="<?php echo $row->productname;?>">
+								<?php echo $row->productname;?></td>
+			<td><input type="hidden" name="productid" value="<?php echo $row->productid;?>">
+								<?php echo $row->productid;?></td>
+			<td><input type="hidden" name="category" value="<?php echo $row->category;?>">
+								<?php echo $row->category;?></td>
+			<td><input type="hidden" name="companyname" value="<?php echo $row->companyname;?>">
+							<?php echo $row->companyname;?></td>
+			<td><input type="hidden" name="price" value="<?php echo $row->price;?>">
+							<?php echo $row->price;?></td>
+			<td><input type="hidden" name="pcities" value="<?php echo $row->pcities;?>">
+							<?php echo $row->pcities;?></td>
+							
+							
+							
+							
+			<td><input type="hidden" name="bquantity" value="<?php echo $row->bquantity;?>">
+		     <?php echo $row->bquantity."/" ;echo $row->bunits;?>
+			 <input type="hidden" name="bunits" value="<?php echo $row->bunits;?>">
+		</td>
+		<td><input type="hidden" name="bprice" value="<?php echo $row->bprice;?>">
+		     <?php echo $row->bprice."/" ;echo $row->bsupplyability;?>
+			 <input type="hidden" name="bsupplyability" value="<?php echo $row->bsupplyability;?>">
+		</td>
+			<td> <a href="<?php echo base_url().'Customer_sellnegoquotation/auc_no/'.urldecode($row->vname)."/".urldecode($row->busername);?>">
+				<i class="fa fa-download" aria-hidden="true"></i></a></td>
 					
+			
+			<td>
+				
+				<input class="form-group w-auto"  multiple="multiple"  type="file" name="upload_dd[]">
+					
+			
+					<input type="submit" id="" class="btn btn-info " name="submit" value="Upload purchase order">
+					
+			
+				
+		 </td>
+		<?php $k++;}?>
       </tr>      
-     <?php }?>	
+      
     </tbody>
   </table>
+  </form>
 		</div>
 		</div>
 		</div>
@@ -87,4 +116,3 @@
 
   </div>
   <!-- End of Page Wrapper -->
-
