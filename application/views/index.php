a3eb9dc5beb402d87672f3009e32701b946afdf6
Preeti <preetisahu0706@gmail.com>
2021-02-04 13:31:50 +0530

    changes in admin

--- application/views/index.php
+++ application/views/index.php
@@ -5,9 +5,10 @@
 		
 		
             <div class="form-group input1">
-                <input type="text" class="form-control" id="gettable_search" onkeyup="search_product()" placeholder="Enter your raw material" >
+                <input type="text" class="form-control" id="productname" onkeyup="search_product()" placeholder="Enter your raw material">
             </div>
 			
+		
 			
 			
             <div class="form-group input2">
@@ -38,8 +39,10 @@
 						  <button type="button" class="btn searchbtn input4"><i class="fa fa-search pr-1"></i>Search</button>
 						</a>
 				</div>
+				
+			
 </form>
-
+	<div id="select" class="select"><ul id="dp"></ul></div>
 		
         <div class="links mt-2">
                <a href="<?php echo base_url();?>#Live Procurement">Live Procurement</a>
@@ -58,7 +61,30 @@
 
     </main>
 		
-            
+   <script>
+ function search_product(){
+	 $("#dp").hide();
+ if(document.getElementById("productname").value.length > 1){
+	 $("#dp").show();
+			var contents = $('#productname').val(); 
+			$.get('<?php echo base_url() .'home_contactsupplier/get_product_table/'; ?>'+contents, function(data){
+				$('#dp').html(data);
+			});
+
+					
+ }else{
+	 return;
+ }
+}
+</script> 
+ <script>
+ function getPaging(v){
+var s = v.split("|");
+	document.getElementById("productname").value  = s[0];
+	document.getElementById("vname").value = s[1];
+	 $("#dp").hide();
+ }
+ </script>        
   
        
 	   
