d2371661e2a8f38638a37fb757de5d130a01b567
preeti <preetisahu0706@gmail.com>
2021-03-13 11:31:57 +0530

    first page of UI of rawmet24

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
 <section id="footer">
 		<div class="container">
-	<div class="row text-center text-xs-center text-sm-left text-md-left">
+<!--	<div class="row text-center text-xs-center text-sm-left text-md-left">
 				<div class="col-xs-12 col-sm-4 col-md-4">
 					<h5>WHAT WE DO</h5>
 					<ul class="list-unstyled quick-links">
@@ -48,7 +48,7 @@
 						</div>
 					</div>
 				</div>
-			</div>
+			</div>-->
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
 				
