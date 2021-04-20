c93ea5aac80f83b6290e9c666cf6c19b54b30f2f
Yuvraj <qwert.yuvraj@gmail.com>
2021-04-20 16:19:25 +0530

    reslove seller product name

--- application/views/vendor/seller_renego_buyresponse.php
+++ application/views/vendor/seller_renego_buyresponse.php
@@ -178,7 +178,7 @@
 	<div class="card shadow mb-4">
     <!-- Card Header - Dropdown -->
 		<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-        <h6 class="m-0 font-weight-bold text-primary">Product Name:<?php echo  $sqldata[0]->productname;?></h6>
+        <h6 class="m-0 font-weight-bold text-primary">Product Name:<?php echo  $sqldata1[0]->productname;?></h6>
 			
         </div>
  <!-- Card Body -->
