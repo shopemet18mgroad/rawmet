2bf968dc7f8b46accca1e20a312ca5d8b451e1ba
unknown <meghasangamesh2@gmail.com>
2021-07-06 14:16:34 +0530

    refund policy

--- application/views/index.php
+++ application/views/index.php
@@ -520,7 +520,7 @@ Get to know us better by connecting with us on our Facebook, Twitter, Google , Y
             </div>
         </div>
     </div>
-	----
+	
 	<div class="modal fade" id="returnrefundpolicy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
@@ -531,20 +531,76 @@ Get to know us better by connecting with us on our Facebook, Twitter, Google , Y
                         <i class="fa fa-window-close"></i>
                     </button>
                 </div>
-				<div class="modal-body">Thanks for subscribing to our services at RAWMET24.Com operated by
-SHOPEMET NETWORKS PRIVATE LIMITED.</div>
-<div class="modal-body">
-We don't offer refund on subscription amount for the service offered on our website
-</div>
- 
-                <div class="modal-body"><h6>Please read the following statement about our information gathering and dissemination practices.</h6>
-				
-				<h6 style="color:blue">Note: Our Return and Refund policy is subject to change at any time without notice. To make sure you are aware of any changes, please review this policy periodically.</h6>By visiting this Website www.rawmet24.com you agree to be bound by the terms and conditions of this Privacy Policy. If you do not agree please do not use or access our Website.<br/>
-				 This Return and Refund policy is incorporated into and subject to the Terms of Use.</div>
-				 
-				
-				 
-				 
+			
+	<div class="modal-body">			 
+			<h1>Return and Refund Policy</h1>
+<p>Last updated: July 06, 2021</p>
+<p>Thank you for shopping at RawMet24.</p>
+<p>If, for any reason, You are not completely satisfied with a purchase We invite You to review our policy on refunds and returns.</p>
+<p>The following terms are applicable for any products that You purchased with Us.</p>
+<h1>Interpretation and Definitions</h1>
+<h2>Interpretation</h2>
+<p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
+<h2>Definitions</h2>
+<p>For the purposes of this Return and Refund Policy:</p>
+<ul>
+<li>
+<p><strong>Company</strong> (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Agreement) refers to SHOPEMET NETWORKS PVT LTD, #179, 2nd Floor, Mysore Road Cross, Bangalore - 560 002.</p>
+</li>
+<li>
+<p><strong>Goods</strong> refer to the items offered for sale on the Service.</p>
+</li>
+<li>
+<p><strong>Orders</strong> mean a request by You to purchase Goods from Us.</p>
+</li>
+<li>
+<p><strong>Service</strong> refers to the Website.</p>
+</li>
+<li>
+<p><strong>Website</strong> refers to RawMet24, accessible from <a href="www.rawmet24.com" rel="external nofollow noopener" target="_blank">www.rawmet24.com</a></p>
+</li>
+<li>
+<p><strong>You</strong> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</p>
+</li>
+</ul>
+<h1>Your Order Cancellation Rights</h1>
+<p>You are entitled to cancel Your Order within 7 days without giving any reason for doing so.</p>
+<p>The deadline for cancelling an Order is 7 days from the date on which You received the Goods or on which a third party you have appointed, who is not the carrier, takes possession of the product delivered.</p>
+<p>In order to exercise Your right of cancellation, You must inform Us of your decision by means of a clear statement. You can inform us of your decision by:</p>
+<ul>
+<li>By email: admin@shopemet.com</li>
+</ul>
+<p>We will reimburse You no later than 14 days from the day on which We receive the returned Goods. We will use the same means of payment as You used for the Order, and You will not incur any fees for such reimbursement.</p>
+<h1>Conditions for Returns</h1>
+<p>In order for the Goods to be eligible for a return, please make sure that:</p>
+<ul>
+<li>The Goods were purchased in the last 7 days</li>
+<li>The Goods are in the original packaging</li>
+</ul>
+<p>The following Goods cannot be returned:</p>
+<ul>
+<li>The supply of Goods made to Your specifications or clearly personalized.</li>
+<li>The supply of Goods which according to their nature are not suitable to be returned, deteriorate rapidly or where the date of expiry is over.</li>
+<li>The supply of Goods which are not suitable for return due to health protection or hygiene reasons and were unsealed after delivery.</li>
+<li>The supply of Goods which are, after delivery, according to their nature, inseparably mixed with other items.</li>
+</ul>
+<p>We reserve the right to refuse returns of any merchandise that does not meet the above return conditions in our sole discretion.</p>
+<p>Only regular priced Goods may be refunded. Unfortunately, Goods on sale cannot be refunded. This exclusion may not apply to You if it is not permitted by applicable law.</p>
+<h1>Returning Goods</h1>
+<p>You are responsible for the cost and risk of returning the Goods to Us. You should send the Goods at the following address:</p>
+<p>Back to Vendors</p>
+<p>We cannot be held responsible for Goods damaged or lost in return shipment. Therefore, We recommend an insured and trackable mail service. We are unable to issue a refund without actual receipt of the Goods or proof of received return delivery.</p>
+<h1>Gifts</h1>
+<p>If the Goods were marked as a gift when purchased and then shipped directly to you, You'll receive a gift credit for the value of your return. Once the returned product is received, a gift certificate will be mailed to You.</p>
+<p>If the Goods weren't marked as a gift when purchased, or the gift giver had the Order shipped to themselves to give it to You later, We will send the refund to the gift giver.</p>
+<h2>Contact Us</h2>
+<p>If you have any questions about our Returns and Refunds Policy, please contact us:</p>
+<ul>
+<li>By email: admin@shopemet.com</li>
+</ul>
+
+
+</div>				 
 				
 				
 
@@ -584,7 +640,6 @@ We don't offer refund on subscription amount for the service offered on our webs
 	
 	
 	
-	----
 	<div class="modal fade" id="paymentpolicy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
